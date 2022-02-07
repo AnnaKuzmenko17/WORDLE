1bbf9bf3bef2cb2aa0245695909279c08d8edb41
AnnaKuzmenko17 <84227066+AnnaKuzmenko17@users.noreply.github.com>
2022-02-07 22:35:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const PORT = 8000;
 
 const axios = require("axios").default;
 const express = require("express");
-const cors = require("cors")
-
+const cors = require("cors");
+require('dotenv').config();
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.get("/word", (req, res) => {
 
@@ -16,15 +16,15 @@ app.get("/word", (req, res) => {
     params: {count: '5', wordLength: '5'},
     headers: {
       'x-rapidapi-host': 'random-words5.p.rapidapi.com',
-      'x-rapidapi-key': '93776c24b1msh674cfbf3bc0ecf0p107b79jsnfff1543cd0c7'
+      'x-rapidapi-key': process.env.RAPID_API_KEY
     }
   };
   
   axios.request(options).then((response) => {
-    console.log(response.data)
-    res.json(response.data[0])
+    console.log(response.data);
+    res.json(response.data[0]);
   }).catch((error) => {
-    console.error(error)
+    console.error(error);
   });
 })
 app.get('/check', (req, res) => {
@@ -37,7 +37,7 @@ app.get('/check', (req, res) => {
       params: {entry: word},
       headers: {
         'x-rapidapi-host': 'twinword-word-graph-dictionary.p.rapidapi.com',
-        'x-rapidapi-key': '93776c24b1msh674cfbf3bc0ecf0p107b79jsnfff1543cd0c7'
+        'x-rapidapi-key': process.env.RAPID_API_KEY
       }
     };
     
